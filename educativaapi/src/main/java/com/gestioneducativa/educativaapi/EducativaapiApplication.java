6c052ac26eb75a29b01c665e69f078ec3dd2c34f
Bryan Ojeda <limpsket,bra@gmail.com>
2021-09-03 00:22:19 -0500

    primer comit

---
+++ educativaapi/src/main/java/com/gestioneducativa/educativaapi/EducativaapiApplication.java
@@ -0,0 +1,13 @@
+package com.gestioneducativa.educativaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducativaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducativaapiApplication.class, args);
+	}
+
+}
